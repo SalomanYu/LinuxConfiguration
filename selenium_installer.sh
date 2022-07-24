2151350c75e27d331b071c77d5522a9101222c17
SalomanYu <rosya4806@gmail.com>
2022-07-24 23:21:55 +0300

    add selenium installer

---
+++ selenium_installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_version_chrome=$(google-chrome-stable --version | egrep -o "[0-9].*$")
+domain="https://chromedriver.storage.googleapis.com"
+type="chromedriver_linux64.zip"
+
+url="$domain/$current_version_chrome/$type"
+wget $url
+unzip $type
+sudo mv chromedriver /usr/local/bin/chromedriver
+sudo chown root:root !$
+sudo chmod +x !$
+
+#testing
+echo "Testing chromedriver"
+notify-send "ChromeDriver $current_version_chrome installed!"
+chromedriver
+
